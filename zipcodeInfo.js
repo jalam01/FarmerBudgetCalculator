976f4ca470b95358b29e0d9b92e948c096018ca3
Failfer <dceaton9@gmail.com>
2025-03-08 19:26:29 -0800

    Added programatic pricing by zip code

--- zipcodeInfo.js
+++ zipcodeInfo.js
@@ -4,25 +4,60 @@ const mockData = {
     '91962': { water_per_foot_acre: 65.0 },
     '91340': { water_per_foot_acre: 85.0 }
   };
-  
-  // Mock Eco-Friendly Suggestions
-  const ecoAlternatives = {
-    'Drip irrigation': { rank: '#1', eco: 'Best Option' },
-    'Central Pivot Irrigation': { rank: '#2', eco: 'Drip irrigation' },
-    'Sprinkler': { rank: '#3', eco: 'Central Pivot Irrigation' },
-    'Flood irrigation': { rank: '#4', eco: 'Sprinkler' },
+
+  const irrigationEfficiency = { 
+    //How water efficent it is (fraction) or how much water isnt lost to evaporation
+    "Sprinklers": 0.70,
+    "Drip_Irrigation": 0.95,
+    "Flood_Irrigation": 0.52,
+    "Center_Pivot":0.88
+
   };
+  
           
   // Function to handle pricing based on ZIP code
   function getPricing() {
     const zipcode = document.getElementById("zipcode").value;
     const pricingPerFA = mockData[zipcode]; //price per Foot Acre for this zip code
-  
-  
-    if (pricing) {
+    
+
+    if (pricingPerFA) {
+      var m1 = document.getElementById("current_method");
+      var m2 = document.getElementById("other_method");
+
+      var m1Loss = 999999;
+      var m2Loss = 999999;
+      
+      if (m1.value == "Sprinklers"){
+        m1Loss = pricingPerFA.water_per_foot_acre * irrigationEfficiency.Sprinklers;
+        
+        
+      }
+      else if(m1.value == "Drip Irrigation"){
+        m1Loss = pricingPerFA.water_per_foot_acre * irrigationEfficiency.Drip_Irrigation;
+
+      }
+      else if(m1.value == "Flood Irrigation"){
+        m1Loss = pricingPerFA.water_per_foot_acre * irrigationEfficiency.Flood_Irrigation;
+
+      }
+      else if(m1.value == "Center Pivot"){
+        m1Loss = pricingPerFA.water_per_foot_acre * irrigationEfficiency.Center_Pivot;
+
+      }
+      console.log(m1Loss);
+
+      
+
+
+
+
+
+
+
+
       document.getElementById("pricing-results").innerHTML = `
-        <p>Local Sales Tax: ${pricing.sales_tax * 100}%</p>
-        <p>Material Cost: $${pricing.material_cost}</p>
+
       `;
     } else {
       document.getElementById("pricing-results").innerHTML = "<p>No data available for this ZIP code.</p>";
