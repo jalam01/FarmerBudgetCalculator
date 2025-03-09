95ddfdc30b8bd325e6f4f9075d0c695640b32d8b
jalam01 <162780469+jalam01@users.noreply.github.com>
2025-03-08 20:43:05 -0800

    Final fix up
    This took a bit.

--- zipcodeInfo.js
+++ zipcodeInfo.js
@@ -28,4 +28,48 @@ const mockData = {
     const zipcode = document.getElementById("zipcode").value;
     const pricingPerFA = mockData[zipcode]; //price per Foot Acre for this zip code
   }
+
+  if (pricingPerFA) {
+    var m1 = document.getElementById("current_method");
+    var m2 = document.getElementById("other_method");
+
+    var m1Loss = 999999;
+    var m2Loss = 999999;
+    
+    if (m1.value == "Sprinklers"){
+      m1Loss = pricingPerFA.water_per_foot_acre * irrigationEfficiency.Sprinklers;
+      
+      
+    }
+    else if(m1.value == "Drip Irrigation"){
+      m1Loss = pricingPerFA.water_per_foot_acre * irrigationEfficiency.Drip_Irrigation;
+
+    }
+    else if(m1.value == "Flood Irrigation"){
+      m1Loss = pricingPerFA.water_per_foot_acre * irrigationEfficiency.Flood_Irrigation;
+
+    }
+    else if(m1.value == "Center Pivot"){
+      m1Loss = pricingPerFA.water_per_foot_acre * irrigationEfficiency.Center_Pivot;
+
+    }
+    console.log(m1Loss);
+
+    
+
+
+
+
+
+
+
+
+    document.getElementById("pricing-results").innerHTML = `
+
+    `;
+  } else {
+    document.getElementById("pricing-results").innerHTML = "<p>No data available for this ZIP code.</p>";
+  }
+
+
   
